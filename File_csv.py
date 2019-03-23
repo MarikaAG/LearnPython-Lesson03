48ecb461c97b4da0e001c0c76f6571b3aa2ee9c9
Marina <marina_a_g@mail.ru>
2019-03-23 12:21:44 +0300

    All tasks of the week 3

--- File_csv.py
+++ File_csv.py
@@ -6,8 +6,17 @@
 #    ]
 #Запишите содержимое списка словарей в файл в формате csv
 
+import csv
+
 PEOPLE_LIST=[{'name': 'Маша', 'age': 25, 'job': 'Scientist'}, 
              {'name': 'Вася', 'age': 8, 'job': 'Programmer'}, 
              {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'}]
 
 #MAIN
+with open('people.csv', 'w', encoding='utf-8') as csv_file:
+    fields=['name','age','job']
+    set_to_write=csv.DictWriter(csv_file, fields, delimiter=';')
+    set_to_write.writeheader()
+    for person in PEOPLE_LIST:
+        set_to_write.writerow(person)
+
