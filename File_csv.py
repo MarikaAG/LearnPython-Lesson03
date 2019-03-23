65bc41467f7f101b690c5d4414a7019013ea030b
Marina <marina_a_g@mail.ru>
2019-03-23 12:22:53 +0300

    Really ALL tasks of the week 3 :)

--- File_csv.py
+++ File_csv.py
@@ -13,7 +13,7 @@ PEOPLE_LIST=[{'name': 'Маша', 'age': 25, 'job': 'Scientist'},
              {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'}]
 
 #MAIN
-with open('people.csv', 'w', encoding='utf-8') as csv_file:
+with open('people.csv', 'w', encoding='utf-8', newline='') as csv_file:
     fields=['name','age','job']
     set_to_write=csv.DictWriter(csv_file, fields, delimiter=';')
     set_to_write.writeheader()
